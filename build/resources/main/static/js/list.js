1e196b4f95df9e8e649feaa31ffb06ef39910020
BOOK-CI93RAR7FF\87062 <22000286@handong.ac.kr>
2025-05-10 21:41:46 +0900

    Frontend-Design 0510

--- build/resources/main/static/js/list.js
+++ build/resources/main/static/js/list.js
@@ -10,14 +10,23 @@ $(function(){
                 tr.append(`<td>${menu.name}</td>`);
                 tr.append(`<td>${menu.price}</td>`);
                 tr.append(`<td>${menu.description}</td>`);
+
+                if (menu.imageUrl) {
+                    tr.append(`<td><img src="${menu.imageUrl}" alt="${menu.name}" width="100"></td>`);
+                } else {
+                    tr.append(`<td>없음</td>`);
+                }
+
+                // 리뷰쓰기 버튼: /board/detail/{menuId} 경로로 이동
                 tr.append(`<td>
-          <button class="reviewBtn" data-id="${menu.id}">리뷰</button>
-        </td>`);
+                    <a href="/board/detail/${menu.id}" class="reviewBtn">리뷰쓰기</a>
+                </td>`);
+
                 tbody.append(tr);
             });
-            $('.reviewBtn').click(function(){
-                window.location.href = '/board/detail/' + $(this).data('id');
-            });
+        },
+        error: function() {
+            alert("메뉴 불러오기 실패");
         }
     });
 });
